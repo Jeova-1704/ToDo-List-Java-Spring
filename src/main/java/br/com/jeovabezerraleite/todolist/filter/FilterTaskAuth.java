c29b6ea3cee102efc65e57d96a3e94ffb88fb2a3
Jeova BL <jeovab115@gmail.com>
2023-10-12 20:12:32 -0300

    feat(tasks): adicionando filtro para validar informacoes

---
+++ src/main/java/br/com/jeovabezerraleite/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,59 @@
+package br.com.jeovabezerraleite.todolist.filter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.jeovabezerraleite.todolist.user.IUserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var serveletPath = request.getServletPath();
+        if (serveletPath.equals("/tasks/")) {
+            //pegar autenticacao (user, password)
+            var authorization =  request.getHeader("Authorization");
+            var authdecoede = authorization.substring("Basic".length()).trim();
+
+            byte[] authoDecode  = Base64.getDecoder().decode(authdecoede);
+            var authString =  new String(authoDecode);
+            System.out.println("autorização: " + authString);
+
+            String[] credenciais = authString.split(":");
+            String username = credenciais[0];
+            String password = credenciais[1];
+
+            //validar usuario
+            var user = this.userRepository.findByUserName(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if(passwordVerify.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+                //segue viagem
+
+                //else(erro retorna que não tem permisao )
+
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
